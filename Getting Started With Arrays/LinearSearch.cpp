e105b0d904ae98964b08dc246d795a76f1f035ef
unnatiS20 <unnatisharma106@gmail.com>
2024-09-09 09:59:07 +0530

    linear search in array

---
+++ Getting Started With Arrays/LinearSearch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+// Function to perform linear search
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index of the target element
+        }
+    }
+    return -1; // Return -1 if the target element is not found
+}
+
+int main() {
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter " << size << " elements of the array:" << endl;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter the element to search for: ";
+    cin >> target;
+
+    int result = linearSearch(arr, size, target);
+
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found in the array" << endl;
+    }
+
+    return 0;
+}
